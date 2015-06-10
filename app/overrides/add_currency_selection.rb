72129f5abd2a7a4eafaabf17ea09fc168b952841
Kolyada <89anisim89@gmail.com>
2015-06-10 13:51:35 +1000

    Update add_currency_selection.rb

--- app/overrides/add_currency_selection.rb
+++ app/overrides/add_currency_selection.rb
@@ -1,7 +1,7 @@
 # encoding: utf-8
 Deface::Override.new(virtual_path: "spree/shared/_main_nav_bar",
                      name: "currencies_admin_configurations_menu",
-                     insert_bottom: "ul#main-nav-bar",
+                     insert_bottom: "ul.navbar-nav",
                      disabled: false,
                      text: "
                      <li><%= select_tag 'currency', options_for_select(Spree::Currency.all_currencies, Spree::Currency.current.char_code) %></li>
